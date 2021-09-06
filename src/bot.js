b4ba442ba3f3301d707bae991c3b8b39ebaa81e6
snapdeus <snapdeus@gmail.com>
2021-09-06 17:40:09 -0400

    message replying to channel command prefix defined

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,26 @@
 require('dotenv').config();
 
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const PREFIX = "$"
 
-client.on('ready', () => {
+client.once('ready', () => {
     console.log(`${ client.user.tag } has logged in.`)
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = message.content
+            .toLowerCase()
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);
+        if (CMD_NAME === 'kick') {
+            message.channel.send('Kicked the User')
+        }
+
+    }
 });
 
 client.login(process.env.DISCORDJS_BOT_TOKEN)
